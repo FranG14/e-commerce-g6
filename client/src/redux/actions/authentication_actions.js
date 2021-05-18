342e8689615d73b5fc2ff2cb45796bfed808f75b
Francisco Granja <franbbr110@gmail.com>
2021-05-18 20:46:14 -0300

    Deploy y Mensajes Log In y Register

--- client/src/redux/actions/authentication_actions.js
+++ client/src/redux/actions/authentication_actions.js
@@ -21,7 +21,7 @@ export const login = (formData, history) => async (dispatch) => {
         })
         localStorage.setItem('profile', JSON.stringify(u.data))
     })
-    .then(history.push('/'))
+    // .then(history.push('/'))
     .catch ((error) => {
         dispatch({
             type:LOGIN_ERROR,
@@ -42,7 +42,7 @@ export const register = (formData, history) => async (dispatch) => {
         })
         localStorage.setItem('profile', JSON.stringify(u.data))
     })
-    .then(history.push('/'))
+    // .then(history.push('/'))
     .catch((error)=> {
         dispatch({
             type:REGISTER_ERROR,
@@ -72,7 +72,7 @@ export const googleLogIn = (formData, history) => async(dispatch) => {
         })
         localStorage.setItem('profile', JSON.stringify(u.data))
     })
-    .then(history.push('/'))
+    // .then(history.push('/'))
     .catch((error)=>{
         dispatch({
             type:GOOGLE_LOGIN_ERROR,
