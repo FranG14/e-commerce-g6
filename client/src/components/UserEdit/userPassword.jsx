d6f768599b15d7a4f410e0b9662b270ccc195c5c
Francisco Granja <franbbr110@gmail.com>
2021-05-24 19:48:46 -0300

    Verificacion de contraseña en editPassword

--- client/src/components/UserEdit/userPassword.jsx
+++ client/src/components/UserEdit/userPassword.jsx
@@ -18,6 +18,7 @@ const UserPassword = () => {
         _id: id,
         oldPassword: "",
         newPassword: "",
+        repeatPassword: "",
 
     };
     const [password, setPassword] = useState(newPassword);
@@ -29,6 +30,17 @@ const UserPassword = () => {
 
     console.log(passwordArray)
 
+    function verifyPassword(e) {
+        //e.preventDefault()
+        //setState({...state, [e.target.name]: e.target.value})
+        if ((e.target.name === "repeatPassword" && password.newPassword === e.target.value) || (e.target.name === "newPassword" && password.repeatPassword === e.target.value)) {
+            //console.log('Las contraseñas coinciden');
+            setPassword({ ...password, [e.target.name]: e.target.value, equal: true })
+        } else if (e.target.name === "repeatPassword" || e.target.name === "newPassword") {
+            //console.log('Las contraseñas no equal');
+            setPassword({ ...password, [e.target.name]: e.target.value, equal: false })
+        }
+    }
 
     const handleInputChange = (e) => {
         setPassword({
@@ -38,36 +50,42 @@ const UserPassword = () => {
     };
 
     const handleSubmit = (e) => {
+        if (
+            password.newPassword === password.repeatPassword) {
 
-        //e.preventDefault();
-        const passwordSend = {
-            id: password._id,
-            oldPassword: password.oldPassword,
-            newPassword: password.newPassword
+            //e.preventDefault();
+            const passwordSend = {
+                id: password._id,
+                oldPassword: password.oldPassword,
+                newPassword: password.repeatPassword
 
-        };
-        /*  if(password.name === '') return swal({
-             title: "Name Field Cannot Be Empty",
-             icon: "warning",
-             button: true,
-             dangerMode: true,
-         })
-           if(password.description === '') return  swal({
-             title: "Description Field Cannot Be Empty",
-             icon: "warning",
-             button: true,
-             dangerMode: true, */
-        /*  }) */
-        dispatch(editPassword(password._id, passwordSend));
-        setPassword(newPassword)
-        dispatch({ type: "LOGOUT" })
-        history.push('/auth')
+            };
+            /*  if(password.name === '') return swal({
+                 title: "Name Field Cannot Be Empty",
+                 icon: "warning",
+                 button: true,
+                 dangerMode: true,
+             })
+               if(password.description === '') return  swal({
+                 title: "Description Field Cannot Be Empty",
+                 icon: "warning",
+                 button: true,
+                 dangerMode: true, */
+            /*  }) */
+            dispatch(editPassword(id, passwordSend));
+            setPassword(newPassword)
+            dispatch({ type: "LOGOUT" })
+            history.push('/auth')
 
-        swal("Password Changed!", "Well done!", "success", { button: true }).then(function () {
-            // window.location.replace(`https://e-commerce-g6.netlify.app/`)
-            history.push(`/auth`)
-        });
-    };
+            swal("Password Changed!", "Well done!", "success", { button: true }).then(function () {
+                // window.location.replace(`https://e-commerce-g6.netlify.app/`)
+                history.push(`/auth`)
+            });
+        } else {
+            swal("Password doesn't match", "Try Again", "warning", { button: true })
+        }
+        // window.location.replace(`https://e-commerce-g6.netlify.app/`)
+    }
     // console.log(product)
     return (
         <div class="  bg-gray-200">
@@ -102,7 +120,7 @@ const UserPassword = () => {
                                             type="password"
                                             name="newPassword"
                                             value={password.newPassword}
-                                            onChange={handleInputChange}
+                                            onChange={verifyPassword}
                                             placeholder="New "
                                             class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none 
                                 focus:ring focus:ring-indigo-100 
@@ -110,11 +128,32 @@ const UserPassword = () => {
                                 dark:text-white dark:placeholder-gray-500 
                                 dark:border-gray-600 dark:focus:ring-gray-900 
                                 dark:focus:border-gray-500"
+                                            required
                                         />
 
                                     </div>
                                     <div class="mb-6">
-                                        <button type="submit" onClick={handleSubmit} class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Edit</button>
+                                        <label for="newPassword" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">New Password</label>
+                                        <input
+                                            id="newPassword"
+                                            type="password"
+                                            name="repeatPassword"
+                                            value={password.repeatPassword}
+                                            onChange={verifyPassword}
+                                            placeholder="New "
+                                            class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none 
+                                focus:ring focus:ring-indigo-100 
+                                focus:border-indigo-300 dark:bg-gray-700 
+                                dark:text-white dark:placeholder-gray-500 
+                                dark:border-gray-600 dark:focus:ring-gray-900 
+                                dark:focus:border-gray-500"
+                                            required
+                                        />
+
+
+                                    </div>
+                                    <div class="mb-6">
+                                        <button type="button" onClick={handleSubmit} class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Edit</button>
                                     </div>
                                 </form>
                             </div>
